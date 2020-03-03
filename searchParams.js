13129f32497d1d53ecd3f95ab4b4384a6456d16f
djarrel <jeffdarrellthegreat@gmail.com>
2020-03-03 21:29:11 +0300

    adopt pets

--- searchParams.js
+++ searchParams.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
+import dropDown from './dropdown'
 
 const SearchParams = () => {
     // replace location
     const [ location, updateLocation ] = useState("Seattle, WA")
+    const [breeds, updateBreeds] = useState([])
+    const [ animal, AnimalDropdown ] = dropDown("Animal", "bird", ANIMALS);
+    const [breed, BreedDropdown, updateBreed] = dropDown("Breed", "", breeds);
+
+useEffect(() => {
+    updateBreeds([]);
+    updateBreed('');
+    pet.breeds(animal).then(({ breeds }) => {
+        const breedItems = breeds.map(({ name }) => name);
+        updateBreeds(breedItems)
+    }, console.error)
+}, [animal, updateBreed, updateBreeds])
+
     return (
         <div className="search-params">
             <form>
@@ -16,6 +31,8 @@ const SearchParams = () => {
                     (e.target.value)}
                 />
                 </label>
+                    <AnimalDropdown/>
+                    <BreedDropdown/>
                 <button>submit</button>
             </form>
         </div>
